60d0436558e56eb499c437259f89921368c64391
Mark Powers <markppowers0@gmail.com>
2019-03-22 17:26:41 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,99 @@
+const server = require('./server');
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+
+const dbCreds = config.database;
+const secret = config.jwt_secret;
+
+const jwtFunctions = {
+  sign: function (message) {
+    return jwt.sign({ value: message }, secret);
+  },
+  verify: function (token) {
+    return jwt.verify(token, secret).value;
+  }
+}
+
+const database = new Sequelize(dbCreds.database, dbCreds.user, dbCreds.password, {
+  logging(str) {
+    console.debug(`DB:${str}`);
+  },
+  dialectOptions: {
+    charset: 'utf8mb4',
+    multipleStatements: true,
+  },
+  //   host: dbCreds.host,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+database.authenticate().then(() => {
+  console.debug(`database connection successful: ${dbCreds.database}`);
+}, (e) => console.log(e));
+
+async function sync(alter, force, callback) {
+  await database.sync({ alter, force, logging: console.log });
+}
+
+function setUpModels() {
+  const models = {
+    "movies": database.define('movies', {
+      title: {
+        type: Sequelize.TEXT,
+      },
+      director: {
+        type: Sequelize.TEXT,
+      },
+      year: {
+        type: Sequelize.INTEGER,
+      }
+    },
+      { timestamps: false, }),
+    "books": database.define('books', {
+      title: {
+        type: Sequelize.TEXT,
+      },
+      author: {
+        type: Sequelize.TEXT,
+      },
+      year: {
+        type: Sequelize.INTEGER,
+      },
+      is_read: {
+        type: Sequelize.BOOLEAN
+      },
+      is_owned: {
+        type: Sequelize.BOOLEAN
+      }
+    },
+      { timestamps: false, }),
+    "todos": database.define('todos', {
+      type: {
+        type: Sequelize.TEXT,
+      },
+      title: {
+        type: Sequelize.TEXT,
+      },
+      notes: {
+        type: Sequelize.TEXT,
+      },
+    },
+      { timestamps: false, }),
+  }
+
+  return models;
+}
+
+const models = setUpModels();
+sync();
+
+server.setUpRoutes(models, jwtFunctions, database);
+server.listen(config.port);
+
---
+++ src/server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+server.use(bodyParser.json());
+
+function listen(port) {
+    server.listen(port, () => console.info(`Listening on port ${port}!`));
+}
+
+function setUpRoutes(models, jwtFunctions, database) {
+    server.use(function (req, res, next) {
+        if(req.ip.endsWith("127.0.0.1")){
+            next()
+        } else {
+            res.status(400).send("Cannot connect remotely");
+        }
+    })
+    // Route logging
+    server.use(function (req, res, next) {
+        console.debug(new Date(), req.method, req.originalUrl);
+        next()
+    })
+
+    server.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+    var getTable = async function(name, req, res, next, orderby = "title"){
+        try {
+            var result = await database.query(`SELECT * FROM ${name} order by ${orderby}`, { type: database.QueryTypes.SELECT })
+            res.status(200).send(result);
+            next();
+        } catch (e) {
+            console.log(e)
+            res.status(400).send(e.message);
+        }
+    };
+    var postTable = async function(name, req, res, next, orderby = "title"){
+        try{
+            let item = req.body;
+            console.log(item);
+            await models[name].create(item);
+            var result = await database.query(`SELECT * FROM ${name} order by ${orderby}`, { type: database.QueryTypes.SELECT })
+            res.status(200).send(result);
+        } catch (e) {
+            console.log(e);
+            res.status(400).send(e.message);
+        }
+    }
+    var deleteTable = async function(name, req, res, next, orderby = "title"){
+        try{
+            let id = req.body.id;
+            console.log(`Deleting ${id}`);
+            await models[name].destroy({where: {id: id}});
+            var result = await database.query(`SELECT * FROM ${name} order by ${orderby}`, { type: database.QueryTypes.SELECT })
+            res.status(200).send(result);
+        } catch (e) {
+            console.log(e);
+            res.status(400).send(e.message);
+        }
+    }
+
+    server.get('/movies', async (req, res, next) => {
+        await getTable('movies', req, res, next);
+    })
+    server.post('/movies', async (req, res, next) => {
+        await postTable('movies', req, res, next);
+    })
+    server.delete('/movies', async (req, res, next) => {
+        await deleteTable('movies', req, res, next);
+    })
+    server.get('/books', async (req, res, next) => {
+        await getTable('books', req, res, next);
+    })
+    server.post('/books', async (req, res, next) => {
+        await postTable('books', req, res, next);
+    })
+    server.delete('/books', async (req, res, next) => {
+        await deleteTable('books', req, res, next);
+    })
+    server.get('/todos', async (req, res, next) => {
+        await getTable('todos', req, res, next, "type, title");
+    })
+    server.post('/todos', async (req, res, next) => {
+        await postTable('todos', req, res, next, "type, title");
+    })
+    server.delete('/todos', async (req, res, next) => {
+        await deleteTable('todos', req, res, next, "type, title");
+    })
+
+}
+
+module.exports = {
+    listen,
+    setUpRoutes
+};
+
+
